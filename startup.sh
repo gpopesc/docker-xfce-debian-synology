85e9f9f64c2899ea97a3640eadbd892746e6c43c
gpopesc <gpopesc@gmail.com>
2021-07-15 12:41:01 +0300

    Update startup.sh
    added script for chromium

--- startup.sh
+++ startup.sh
@@ -5,6 +5,13 @@ curl -fsSL https://download.opensuse.org/repositories/home:stevenpusser/Debian_1
 echo 'deb http://download.opensuse.org/repositories/home:/Alexx2000/Debian_10/ /' | sudo tee /etc/apt/sources.list.d/home:Alexx2000.list
 curl -fsSL https://download.opensuse.org/repositories/home:Alexx2000/Debian_10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_Alexx2000.gpg > /dev/null
 
+#adjust chromium shortcut for running
+if [ -e "/usr/share/applications/chromium.desktop" ]; then
+     sed -i 's|Exec=/usr/bin/chromium %U|Exec=/usr/bin/chromium --no-sandbox %U|g' /usr/share/applications/chromium.desktop
+else
+echo "No Chromium installed"
+fi
+
 #alllow bash script running from thunar
-sleep 60
+sleep 30
 xfconf-query --channel thunar --property /misc-exec-shell-scripts-by-default --create --type bool --set true
