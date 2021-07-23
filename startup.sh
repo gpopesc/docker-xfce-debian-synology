eb8874e777a00392786ea1a4dad2ffcc5e6818a0
gpopesc <gpopesc@gmail.com>
2021-07-23 10:32:52 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+LOG_LOCATION=/tmp/
+exec >> $LOG_LOCATION/mylogfile.log 2>&1
 #add steven repository, enable autoupdate for palemoon
 echo 'deb http://download.opensuse.org/repositories/home:/stevenpusser/Debian_10/ /' | tee /etc/apt/sources.list.d/home:stevenpusser.list 
 curl -fsSL https://download.opensuse.org/repositories/home:stevenpusser/Debian_10/Release.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/home_stevenpusser.gpg > /dev/null
