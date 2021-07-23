cabe4945b378c39c903950069511923cac1559a9
gpopesc <gpopesc@gmail.com>
2021-07-23 10:53:02 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -16,7 +16,7 @@ if [ -n "${USER_NAME}" ] || [ "${USER_NAME}"!='none' ]
   useradd -m -p $(openssl passwd -1 ${USER_PASSWORD}) -s /bin/bash -G sudo ${USER_NAME}
   sudo usermod -a -G root ${USER_NAME}
   export HOME=/home/${USER_NAME}
-  mkdir /home/${USER_NAME}/.config/xfce4/xfconf/xfce-perchannel-xml
+  mkdir -p /home/${USER_NAME}/.config/xfce4/xfconf/xfce-perchannel-xml
   cp /tmp/xfce4-panel.xml /home/${USER_NAME}/.config/xfce4/xfconf/xfce-perchannel-xml
   echo ${USER_PASSWORD} | sudo -u ${USER_NAME} -S cp /tmp/xfce4-panel.xml /home/${USER_NAME}/.config/xfce4/xfconf/xfce-perchannel-xml
   cp /root/capslock_toggle.sh /home/${USER_NAME}/capslock_toggle.sh
