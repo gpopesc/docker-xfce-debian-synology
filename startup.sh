f4ccb835b8a59508122ee7515e15ed831293779b
gpopesc <gpopesc@gmail.com>
2021-07-23 16:12:52 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -33,12 +33,12 @@ if [ -n "${USER_NAME}" ]
   #echo ${USER_PASSWORD} | sudo -u ${USER_NAME} -S chown ${USER_NAME}:0 /home/${USER_NAME}/capslock_toggle.sh
   echo "cd /home/${USER_NAME}" >> ~/.bashrc
   sudo -u ${USER_NAME} startxfce4 & \
-  echo ${USER_PASSWORD} | sudo -u ${USER_NAME} -S  read -p "wait 30s" -t 30 && echo "script finnished"
+  echo ${USER_PASSWORD} | sudo -u ${USER_NAME} -S  sleep 30 && echo "script finnished"
  else
   echo "Running as root"
   mkdir -p /root/.config/xfce4/xfconf/xfce-perchannel-xml
   cp /tmp/*.xml /root/.config/xfce4/xfconf/xfce-perchannel-xml/
   nohup startxfce4 &>/dev/null &
   #allow bash script running from thunar
-  read -p "wait 30s" -t 30 && xfconf-query --channel thunar --property /misc-exec-shell-scripts-by-default --create --type bool --set true && echo "script finnished"
+  sleep 30 && xfconf-query --channel thunar --property /misc-exec-shell-scripts-by-default --create --type bool --set true && echo "script finnished"
 fi
